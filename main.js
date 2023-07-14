2bbe36e465d0bf4cb9940a2b34f573118c05848d
xu <xuaiwen11@gmail.com>
2023-07-14 17:25:27 +0900

    修改navbar点击背景

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var na = document.querySelectorAll(".nav > div a")
+var da = document.querySelectorAll(".nav > div")
 for (var i = 0; i < na.length; i++)
   na[i].addEventListener("click", function () {
     for (var j = 0; j < na.length; j++) na[j].style.color = "white"
     this.style.color = "black"
+  })
+for (var k = 0; k < na.length; k++)
+  da[k].addEventListener("click", function () {
+    for (var o = 0; o < na.length; o++) da[o].style.backgroundColor = ""
+    this.style.backgroundColor = "rgba(200, 16, 47, 0.5)"
   })
