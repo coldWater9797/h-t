4f51b323681e0988495cbf8c62abe5a7a0c8ddf4
xu <xuaiwen11@gmail.com>
2023-07-19 10:45:42 +0900

    contact us背景图，scrollToTop按钮

--- main.js
+++ main.js
@@ -10,3 +10,40 @@ for (var k = 0; k < na.length; k++)
     for (var o = 0; o < na.length; o++) da[o].style.backgroundColor = ""
     this.style.backgroundColor = "rgba(200, 16, 47, 0.5)"
   })
+
+  // document.querySelector('.top').addEventListener('click', function() {
+  //   window.scrollTo({ top: 0, behavior: 'smooth' });
+  // });
+ 
+    //Check the scroll position
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 100) {
+		event.preventDefault();
+		$(".scrollToTop").css({'transform': 'translate(0px, 0px)'});
+	} else {
+		$(".scrollToTop").css({'transform': 'translate(80px, 0px)'});
+	}
+});
+
+
+//Click and scrolls top effect
+$('.scrollToTop').click(function(){
+  $('html, body').animate({scrollTop : 0},800);
+  return false;
+  });
+
+
+// function that detects if the scroll has stopped
+  $.fn.scrollEnd = function(callback, timeout) {
+    $(this).scroll(function(){
+      var $this = $(this);
+      if ($this.data('scrollTimeout')) {
+        clearTimeout($this.data('scrollTimeout'));
+      }
+      $this.data('scrollTimeout', setTimeout(callback,timeout));
+    });
+  };
+  //If the function has stopped, we hide the scroll btn
+  $(window).scrollEnd(function(){
+    $(".scrollToTop").css({'transform': 'translate(80px, 0px)'});
+  }, 1500);
