cf8b3c34ca589a5b9731a1c70400101fa42864e5
xu <xuaiwen11@gmail.com>
2023-07-20 14:51:52 +0900

    Expandable News

--- main.js
+++ main.js
@@ -52,3 +52,61 @@ $.fn.scrollEnd = function (callback, timeout) {
 $(window).scrollEnd(function () {
   $(".scrollToTop").css({ transform: "translate(80px, 0px)" })
 }, 1500)
+$expandable_content = $('.expandable-content');
+expand_animation_time = 1000;
+
+var calc_expandable_content = function() {
+  $expandable_content.each(function() {
+    var $container = $(this);
+    var $toggle = $container.find('.toggle-content');
+    var $excerpt_height = $container.find('.excerpt').outerHeight(true);
+
+    // set the min-height on the viewport element and recalc 
+    // all curret elements
+    $container.find('.viewport').css('max-height', $excerpt_height);
+    set_expandable_content_heights($container);
+
+    // first unbind any existing click handler
+    // then register the new one
+    $toggle.unbind('click').click(function() {
+      var $container = $(this).closest('.expandable-content');
+      $container.toggleClass('open');
+      set_expandable_content_heights($container);
+    });
+  });
+}
+var set_expandable_content_heights = function($target) {
+  var $excerpt = $target.find('.excerpt');
+  var $content = $target.find('.content');
+  var $viewport = $target.find('.viewport');
+  
+  if ( $target.hasClass('open') ) {
+    $viewport.css({
+      'max-height': $content.find('.inner').outerHeight(true)
+    });
+
+    window.setTimeout(function() {
+      $target.addClass('animated');
+      $viewport.css({
+        'min-height': $content.find('.inner').outerHeight(true)
+      });
+    }, expand_animation_time);
+  } else {
+    $viewport.css({
+      'max-height': $excerpt.find('.inner').outerHeight(true),
+      'min-height': $excerpt.find('.inner').outerHeight(true)
+    });
+
+    window.setTimeout(function() {
+      $target.removeClass('animated');
+    }, expand_animation_time);
+  }
+}
+
+calc_expandable_content();
+
+// you should find a more performant way to
+// do this. Throttle or debounce.
+$(window).resize(function() {
+  calc_expandable_content();
+})
